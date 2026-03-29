061c420c62e51ab6ee5ee73b9f784b1509b229ba
Armen Arsakian <aarsakian@gmail.com>
2026-03-29 17:45:15 +0300

    retrieve data implemented

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	evidencePath := flag.String("evidence", "", "Path to the evidence file (VHDX)")
-	//offset := flag.Int64("offset", 0, "offset to read data from the evidence")
-	//length := flag.Int64("len", 0, "number of bytes to read from offset in the evidence")
+	offset := flag.Int64("offset", 0, "offset to read data from the evidence")
+	length := flag.Int64("len", 0, "number of bytes to read from offset in the evidence")
 	flag.Parse()
 
 	if *evidencePath == "" {
@@ -20,6 +20,13 @@ func main() {
 	err := vhdx.ParseEvidence(*evidencePath)
 	if err != nil {
 		log.Fatalf("Failed to parse evidence: %v", err)
+	}
+	if *offset >= 0 && *length > 0 {
+		data, err := vhdx.RetrieveData(*offset, *length)
+		if err != nil {
+			log.Fatalf("Failed to read data: %v", err)
+		}
+		log.Printf("Read %d bytes from offset %d", len(data), *offset)
 	}
 
 	defer vhdx.Close()
