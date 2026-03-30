e15937614b7494e924b556f3ef1c251efeebdd77
Armen Arsakian <aarsakian@gmail.com>
2026-03-30 23:23:55 +0300

    new flag and functionality to write raw data

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	evidencePath := flag.String("evidence", "", "Path to the evidence file (VHDX)")
 	offset := flag.Int64("offset", 0, "offset to read data from the evidence")
 	length := flag.Int64("len", 0, "number of bytes to read from offset in the evidence")
+	out := flag.String("out", "", "filename to write raw data")
+
 	flag.Parse()
 
 	if *evidencePath == "" {
@@ -21,12 +23,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to parse evidence: %v", err)
 	}
-	if *offset >= 0 && *length > 0 {
+
+	if *out == "" && *offset >= 0 && *length > 0 {
 		data, err := vhdx.RetrieveData(*offset, *length)
 		if err != nil {
 			log.Fatalf("Failed to read data: %v", err)
 		}
 		log.Printf("Read %d bytes from offset %d", len(data), *offset)
+	} else if *out != "" && *offset >= 0 && *length > 0 {
+		vhdx.WriteRawFile(*out, *offset, *length)
+		log.Printf("Written %d bytes from offset %d to file %s", *length, *offset, *out)
+	} else {
+		log.Fatal("Please provide valid offset and length using -offset and -len flags, and optionally an output filename using -out flag.")
 	}
 
 	defer vhdx.Close()
